79693ee32fe93c807eb1d95faf3c311ea3f2811a
Eugene Opredelennov <eoprede@gmail.com>
2020-04-12 22:53:24 -0400

    removed unnecessary import

--- run.py
+++ run.py
@@ -6,7 +6,6 @@ from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.common.exceptions import WebDriverException
 import chromedriver_binary
-from datetime import datetime
 
 import config
 from slots import SlotElement
