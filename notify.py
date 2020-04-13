7115b70f106d279a205e1b8e8c62065bb6508646
eoprede <eoprede@users.noreply.github.com>
2020-04-13 17:49:01 -0400

    added notifications and text to speech for Windows and Linux (#15)
    * added notifications and text to speech for Windows and Linux
    
    * meme fixed to be os-aware
    
    Co-authored-by: Eugene Opredelennov <eoprede@gmail.com>

--- notify.py
+++ notify.py
@@ -4,6 +4,8 @@ import toml
 import os
 from random import random
 from twilio.rest import Client as TwilioClient
+import platform
+import time
 
 from config import CONF_PATH
 
@@ -68,10 +70,20 @@ def send_sms(body, conf):
 def alert(message, sound='Blow'):
     log.info("Alerting user with message: '{}'".format(message))
     try:
-        os.popen(
-            'afplay /System/Library/Sounds/{}.aiff && '
-            'say "{}" --rate 150 --voice Fiona'.format(sound, message)
-        )
+        if platform.system() == "Windows":
+            os.popen(
+                    "PowerShell -Command \"Add-Type –AssemblyName System.Speech; (New-Object System.Speech.Synthesis.SpeechSynthesizer).Speak('{}');".format(message)
+                )
+        elif platform.system() == "Linux":
+            # requires speech-dispatcher
+            os.popen(
+                    'spd-say "{}"'.format(message)
+                )
+        else:
+            os.popen(
+                'afplay /System/Library/Sounds/{}.aiff && '
+                'say "{}" --rate 150 --voice Fiona'.format(sound, message)
+            )
     except Exception:
         pass
 
@@ -80,9 +92,13 @@ def annoy():
     """Sorry about it"""
     try:
         for _ in range(15):
-            os.popen(
-                'sleep {} && afplay /System/Library/Sounds/Sosumi.aiff'.format(
+            if platform.system() == "Windows" or platform.system() == "Linux":
+                time.sleep(random())
+                print ('\a')
+            else:
+                os.popen(
+                    'sleep {} && afplay /System/Library/Sounds/Sosumi.aiff'.format(
                     random()
-                ))
+                    ))
     except Exception:
         pass
